c5f4f6789cf8fc59e70e07a728699b3b50ac43ff
Omar <omargeerman@gmail.com>
2017-11-13 23:57:48 +0100

    Refactoring and organizing; Made action generators using ES6 destructuring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,5 +1,28 @@
 import {createStore} from 'redux';
 
+// Action generators - functions that return action objects
+
+const incrementCount = ({ incrementBy = 1} = {}) => ({
+	type: 'INCREMENT',
+	incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1} = {}) => ({
+	type: 'DECREMENT',
+	decrementBy
+});
+
+const setCount = ({ setAmount }) => ({
+	type: 'SET',
+	count: setAmount
+});
+
+const resetCount = () => ({
+	type: 'RESET'
+});
+
+
+
 const store = createStore((state = { count:0 }, action) => {
 	switch (action.type){ 
 		case 'INCREMENT':
@@ -29,27 +52,19 @@ store.subscribe(() => {
 	console.log(store.getState());
 })
 
-store.dispatch({
-	type: 'INCREMENT',
-	incrementBy:5
-});
-store.dispatch({
-	type: 'INCREMENT',
-});
+// store.dispatch({
+// 	type: 'INCREMENT',
+// 	incrementBy:5
+// });
 
-store.dispatch({
-	type: 'RESET'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-	type: 'DECREMENT',
-});
-store.dispatch({
-	type: 'DECREMENT',
-	decrementBy: 10
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-	type: 'SET',
-	count: 101
-})
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({decrementBy: 10}));
+
+store.dispatch(setCount({ setAmount: -100 }))
