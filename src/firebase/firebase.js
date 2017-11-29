036c0bc484a8f0f1c1acaf73092a568dc69436f9
Omar <omargeerman@gmail.com>
2017-11-29 21:49:50 +0100

    Fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import { setTimeout } from 'timers';
 
 var config = {
 	apiKey: "AIzaSyAjK0dMgKWf7dbwta05vWHJnZgG8iC8nro",
@@ -13,33 +14,70 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-	name:"Omar Geerman",
-	age: "31",
-	stressLevel: 6,
-	job:{ 
-		title: 'sofware developer',
-		company: 'google'
-	},
-	location: {
-		city: 'Arnhem',
-		country: 'Netherlands'
-	}
-}).then(() => {
-	console.log('Data is saved!');
-}).catch((e) => {
-	console.log('error: ', e);
-})
+const data = database.ref().on('value', (snapshot) => {
+	const val = snapshot.val();
+	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+});
 
-database.ref().update({
-	stressLevel: 9,
-	'job/company': 'Amazon',
-	'location/city': 'Amsterdam'
-}).then(() => {
-	console.log('updated changes!')
-}).catch((e) => {
-	console.log('error: ', e);
-})
+
+
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+// 		console.log(snapshot.val());
+// 	}, (e) => {
+// 		console.log("error with data fetching ", e)
+// 	});
+
+
+// setTimeout(() => {
+// 	database.ref('age').set(29);
+// }, 3500)
+
+// setTimeout(() => {
+// 	database.ref().off(onValueChange);
+// }, 7000)
+
+// setTimeout(() => {
+// 	database.ref('age').set(30);
+// }, 10500)
+
+// database.ref('location/country')
+// .once('value')
+// .then((snapshot) => {
+// 	const val = snapshot.val();
+// 	console.log(val);
+// })
+// .catch((e) => {
+// 	console.log('error: ', e)
+// })
+
+// database.ref().set({
+// 	name:"Omar Geerman",
+// 	age: "31",
+// 	stressLevel: 6,
+// 	job:{ 
+// 		title: 'sofware developer',
+// 		company: 'google'
+// 	},
+// 	location: {
+// 		city: 'Arnhem',
+// 		country: 'Netherlands'
+// 	}
+// }).then(() => {
+// 	console.log('Data is saved!');
+// }).catch((e) => {
+// 	console.log('error: ', e);
+// })
+
+// database.ref().update({
+// 	stressLevel: 9,
+// 	'job/company': 'Amazon',
+// 	'location/city': 'Amsterdam'
+// }).then(() => {
+// 	console.log('updated changes!')
+// }).catch((e) => {
+// 	console.log('error: ', e);
+// })
 
 // database.ref().remove().then(() => {
 // 	console.log('data was removed');
