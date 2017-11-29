16cb58fb5db8a2f34fddd5e1ea1fe331f3420302
Omar <omargeerman@gmail.com>
2017-11-29 22:27:09 +0100

    Array data in firebase part un

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 import { setTimeout } from 'timers';
+import moment from 'moment';
 
 var config = {
 	apiKey: "AIzaSyAjK0dMgKWf7dbwta05vWHJnZgG8iC8nro",
@@ -14,73 +15,23 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-const data = database.ref().on('value', (snapshot) => {
-	const val = snapshot.val();
-	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+database.ref('expenses').push({
+	description: 'Rent',
+	note: '',
+	amount: 500,
+	createdAt: 1
 });
 
+database.ref('expenses').push({
+	description: 'Gum',
+	note: '',
+	amount: 5,
+	createdAt: 1
+});
 
-
-
-// const onValueChange = database.ref().on('value', (snapshot) => {
-// 		console.log(snapshot.val());
-// 	}, (e) => {
-// 		console.log("error with data fetching ", e)
-// 	});
-
-
-// setTimeout(() => {
-// 	database.ref('age').set(29);
-// }, 3500)
-
-// setTimeout(() => {
-// 	database.ref().off(onValueChange);
-// }, 7000)
-
-// setTimeout(() => {
-// 	database.ref('age').set(30);
-// }, 10500)
-
-// database.ref('location/country')
-// .once('value')
-// .then((snapshot) => {
-// 	const val = snapshot.val();
-// 	console.log(val);
-// })
-// .catch((e) => {
-// 	console.log('error: ', e)
-// })
-
-// database.ref().set({
-// 	name:"Omar Geerman",
-// 	age: "31",
-// 	stressLevel: 6,
-// 	job:{ 
-// 		title: 'sofware developer',
-// 		company: 'google'
-// 	},
-// 	location: {
-// 		city: 'Arnhem',
-// 		country: 'Netherlands'
-// 	}
-// }).then(() => {
-// 	console.log('Data is saved!');
-// }).catch((e) => {
-// 	console.log('error: ', e);
-// })
-
-// database.ref().update({
-// 	stressLevel: 9,
-// 	'job/company': 'Amazon',
-// 	'location/city': 'Amsterdam'
-// }).then(() => {
-// 	console.log('updated changes!')
-// }).catch((e) => {
-// 	console.log('error: ', e);
-// })
-
-// database.ref().remove().then(() => {
-// 	console.log('data was removed');
-// }).catch((e) => {
-// 	console.log('Did not remove data ', e);
-// })
+database.ref('expenses').push({
+	description: 'Water Bill',
+	note: '',
+	amount: 200,
+	createdAt: 1
+});
