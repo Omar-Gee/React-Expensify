3c8d4d6527ea1af03c9632fb3f7922efa47419cd
Omar <omargeerman@gmail.com>
2017-11-30 21:57:01 +0100

    Fetching Expenses: Part II; created actions to set expenses from firebase; added testcases for those actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -46,4 +46,19 @@ export const setExpenses = (expenses) => ({
 });
 
 
-// export const startSetExpenses;
+export const startSetExpenses = () => {
+	// fetch all expense data once
+	return (dispatch) => {
+		return database.ref('expenses').once('value').then((snapshot) => {
+			const expenses =[];
+			snapshot.forEach(childSnapshot => {
+				expenses.push({
+					id: childSnapshot.key,
+					...childSnapshot.val()
+				});
+			});
+			dispatch(setExpenses(expenses));
+		});
+	};
+};
+
