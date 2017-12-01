f40115cb0fb6008910640aa8d481d5c6fbc294ab
Omar <omargeerman@gmail.com>
2017-12-01 13:31:20 +0100

    Logging out

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoginPage from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
+import { startLogin } from '../../actions/auth';
 
 test('should render LoginPage correctly', () => {
-	const wrapper = shallow(<LoginPage />);
+	const wrapper = shallow(<LoginPage startLogin={startLogin}/>);
 	expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on click', () => {
+	const startLogin = jest.fn();
+	const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+	
+	wrapper.find('button').simulate('click');
+	expect(startLogin).toHaveBeenCalled();
 });
